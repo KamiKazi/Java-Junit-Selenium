061b7688a4be70388597db438bb298d80f311dc0
KamiKazi <kamikazi219@gmail.com>
2016-07-22 13:04:32 -0500

    Update SampleSauceCheckBoxTest.java

--- src/test/java/com/yourcompany/Tests/SampleSauceCheckBoxTest.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceCheckBoxTest.java
@@ -27,52 +27,52 @@ public class SampleSauceCheckBoxTest extends SampleSauceTestBase {
      * Runs a simple test verifying the checked checkbox state
      * @throws InvalidElementStateException
      */
-    @Test
-    public void verifyUncheckedCheckBoxInputTest() throws InvalidElementStateException {
+    // @Test
+    // public void verifyUncheckedCheckBoxInputTest() throws InvalidElementStateException {
 
-        //Navigate to the page
-    	driver.get("https://saucelabs.com/test/guinea-pig");
+    //     //Navigate to the page
+    // 	driver.get("https://saucelabs.com/test/guinea-pig");
 
-    	// get page object
-    	GuineaPigPage page = GuineaPigPage.getPage(driver);
+    // 	// get page object
+    // 	GuineaPigPage page = GuineaPigPage.getPage(driver);
 
-        /*
-         checkUncheckedCheckBox is an exposed "service",
-             which interacts with the email input field element by sending text to it.
-        */
-        page.checkUncheckedCheckBox();
+    //     /*
+    //      checkUncheckedCheckBox is an exposed "service",
+    //          which interacts with the email input field element by sending text to it.
+    //     */
+    //     page.checkUncheckedCheckBox();
 
-        /*
-         Assertions should be part of test and not part of Page object.
-         Each test should be verifying one piece of functionality (atomic testing)
-        */
-        assertEquals(page.getUncheckedCheckBoxState(), true);
+    //     /*
+    //      Assertions should be part of test and not part of Page object.
+    //      Each test should be verifying one piece of functionality (atomic testing)
+    //     */
+    //     assertEquals(page.getUncheckedCheckBoxState(), true);
 
-    }
+    // }
 
-    /**
-     * Runs a simple test verifying the checked checkbox state
-     * @throws InvalidElementStateException
-     */
-    @Test
-    public void verifyCheckedCheckBoxInputTest() throws InvalidElementStateException {
+    // /**
+    //  * Runs a simple test verifying the checked checkbox state
+    //  * @throws InvalidElementStateException
+    //  */
+    // @Test
+    // public void verifyCheckedCheckBoxInputTest() throws InvalidElementStateException {
 
-    	driver.get("https://saucelabs.com/test/guinea-pig");
+    // 	driver.get("https://saucelabs.com/test/guinea-pig");
 
-        //Navigate to the page
-        GuineaPigPage page = GuineaPigPage.getPage(driver);
+    //     //Navigate to the page
+    //     GuineaPigPage page = GuineaPigPage.getPage(driver);
 
-        /*
-         checkUncheckedCheckBox is an exposed "service",
-             which interacts with the email input field element by sending text to it.
-        */
-        page.uncheckCheckedCheckBox();
+    //     /*
+    //      checkUncheckedCheckBox is an exposed "service",
+    //          which interacts with the email input field element by sending text to it.
+    //     */
+    //     page.uncheckCheckedCheckBox();
 
-        /*
-         Assertions should be part of test and not part of Page object.
-         Each test should be verifying one piece of functionality (atomic testing)
-        */
-        assertEquals(page.getCheckedCheckBoxState(), false);
+    //     /*
+    //      Assertions should be part of test and not part of Page object.
+    //      Each test should be verifying one piece of functionality (atomic testing)
+    //     */
+    //     assertEquals(page.getCheckedCheckBoxState(), false);
 
-    }
+    // }
 }
