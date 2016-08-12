89894ffc22859c49c821fadd51a5bfe3d9de11ee
KamiKazi <kamikazi219@gmail.com>
2016-08-12 14:00:31 -0500

    Update SampleSauceTextInputTest.java

--- src/test/java/com/yourcompany/Tests/SampleSauceTextInputTest.java
+++ src/test/java/com/yourcompany/Tests/SampleSauceTextInputTest.java
@@ -35,7 +35,7 @@ public class SampleSauceTextInputTest extends SampleSauceTestBase {
         String emailInputText = "abc@gmail.com";
 
         // Navigate to the page
-        driver.get("https://dnav-pp.capitalone.com/#/login");
+        driver.get("https://dnav-pp1b.capitalone.com/#/login");
 
         //assertEquals(page.getEmailText(), emailInputText);
         
